8f478b0db2e2957d123680ccdc8d5a19d9b87ebc
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2011-09-11 08:12:00 +0300

    Adding http-testing context.

---
+++ lib/http_testing.rb
@@ -0,0 +1,4 @@
+module HttpTesting
+  autoload :Context, 'http_testing/context'
+  autoload :VERSION, 'http_testing/version'
+end
---
+++ lib/http_testing/version.rb
@@ -0,0 +1,3 @@
+module HttpTesting
+  VERSION = "0.1.0".freeze
+end
